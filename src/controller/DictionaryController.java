340675f28b25901197fec8cc3e44628ba26fae39
leducanhtai <161910484+leducanhtai@users.noreply.github.com>
2024-10-09 00:22:42 +0700

    update

--- src/controller/DictionaryController.java
+++ src/controller/DictionaryController.java
@@ -14,7 +14,7 @@ public class DictionaryController implements ActionListener{
     private AdminFunction adminFunction;
     private Login login;
     private DisplayUserInfo displayUserInfo;
-    private boolean check = false;
+
     //private DictionaryFuntion dictionaryFuntion;
 
     public DictionaryController(DictionaryView dictionaryView) {
@@ -46,22 +46,23 @@ public class DictionaryController implements ActionListener{
             this.dictionaryView.helpFunction.selectHelp();
             System.out.println("đã ấn help");
         }else if(src.equals("Admin")) {
-            if(this.check == false){
+           // this.login.adminTool();
+           // if(this.login.check == false){
                 this.dictionaryView.login.selectAdmin();
-            }else if(this.check == true) this.login.adminTool();
+           // }else if(this.login.check == true) this.login.adminTool();
             System.out.println("đã ấn admin");
         }else if(src.equals("Đăng nhập")) {
             boolean kt = this.login.checkDangNhap();
-            if(kt){
-                this.check = true;
-                System.out.println(check);
+           // if(kt){
+               // this.login.check = true;
+               // System.out.println(login.check);
                 this.login.jLabel_checkDangNhap.setText(" ");
                 this.login.adminTool();
                 System.out.println(kt);
-            }else if(!kt){
-                System.out.println(kt);
-                this.login.jLabel_checkDangNhap.setText("tên đăng nhâp hoặc mật khẩu sai");
-            }
+//            }else if(!kt){
+//                System.out.println(kt);
+//                this.login.jLabel_checkDangNhap.setText("tên đăng nhâp hoặc mật khẩu sai");
+//            }
         }
 
 
