94fe2e82ce4a9650b27e38ceca7c10b3b59d3072
Thanqnv <161539907+Thanqnv@users.noreply.github.com>
2024-10-23 01:05:11 +0700

    them phuong thuc dieu chinh cac lop

--- src/model/BookModel.java
+++ src/model/BookModel.java
@@ -5,52 +5,63 @@ public class BookModel {
     private String BookName;
     private String Category;
     private String Author;
+    private String Publisher;
     private String Quantity;
 
-    public BookModel(String BookId, String BookName, String Category, String Author, String Quantity) {
+    public BookModel(String BookId, String BookName, String Category, String Author, String Publisher, String Quantity) {
         this.BookId = BookId;
         this.BookName = BookName;
         this.Category = Category;
         this.Author = Author;
-        this.Quantity = Quantity;
-    }
-    public String getBookId() {
-        return BookId;
-    }
-    public void setBookId(String BookId) {
-        this.BookId = BookId;
-    }
-    public String getBookName() {
-        return BookName;
-    }
-    public void setBookName(String BookName) {
-        this.BookName = BookName;
-    }
-    public String getCategory() {
-        return Category;
-    }
-    public void setCategory(String Category) {
-        this.Category = Category;
-    }
-    public String getAuthor() {
-        return Author;
-    }
-    public void setAuthor(String Author) {
-        this.Author = Author;
-    }
-    public String getQuantity() {
-        return Quantity;
-    }
-    public void setQuantity(String Quantity) {
+        this.Publisher = Publisher;
         this.Quantity = Quantity;
     }
 
-    public void setView() {
-        System.out.println("ID: " + this.BookId);
-        System.out.println(this.BookName);
-        System.out.println("Category: " + this.Category);
-        System.out.println("Author: " + this.Author);
-        System.out.println("Quantity: " + this.Quantity);
+    public String getBookId() { return BookId; }
+
+    public void setBookId(String BookId) { this.BookId = BookId; }
+
+    public String getBookName() { return BookName; }
+
+    public void setBookName(String BookName) { this.BookName = BookName; }
+
+    public String getCategory() { return Category; }
+
+    public void setCategory(String Category) { this.Category = Category; }
+
+    public String getAuthor() { return Author; }
+
+    public void setAuthor(String Author) { this.Author = Author; }
+
+    public String getPublisher() { return Publisher; }
+
+    public void setPublisher(String publisher) { this.Publisher = publisher; }
+
+    public String getQuantity() { return Quantity; }
+
+    public void setQuantity(String Quantity) { this.Quantity = Quantity; }
+
+    public static String searchIdBook(String BookName) {
+        // Lay du lieu tu du lieu SACH
     }
-    //////
+
+    public static boolean checkIdBook(String BookId) {
+        // Lay du lieu tu du lieu SACH
+    }
+
+    public static String generateNewBookCode() {
+        // Phuong thuc lay ma so sach lon nhat, them tu dong ma so sach moi
+    }
+
+    public static boolean addBook(String BookId, String BookName, String Category, String Author, String Publisher, String Quantity) {
+        // Them sach vao co so du lieu
+    }
+
+    public static boolean deleteBook(String BookId) {
+        // Xoa sach trong co so du lieu
+    }
+
+    public static boolean updateBook(String BookId, String BookName, String Category, String Author, String Publisher, String Quantity) {
+        // Cap nhat sach trong co so du lieu
+    }
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -3,93 +3,82 @@ package model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Student {
-    private String fullName;
-    private String libraryId;
-    private String username;
-    private String password;
-    private String email;
+    private int idStudent;
+    private String nameStudent;
+    private boolean gender;
+    private LocalDate birthDate;
     private String phone;
-    private int numberOfBookBorrowed;
+    public static ArrayList<Student> students = new ArrayList<Student>();
+    public String genderStr;
 
-    public Student(String fullName,  String username, String password, String email, String phone) {
-        this.fullName = fullName;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.phone = phone;
-        this.libraryId = "";
-        this.numberOfBookBorrowed = 0;
+    public int getIdStudent() { return idStudent; }
+
+    public void setIdStudent(int idStudent) { this.idStudent = idStudent; }
+
+    public String getnameStudent() { return nameStudent; }
+
+    public void setnameStudent(String nameStudent) { this.nameStudent = nameStudent; }
+
+    public boolean isGender() { return gender; }
+
+    public void setGender(boolean gender) { this.gender = gender; }
+
+    public LocalDate getBirthDate() { return birthDate; }
+
+    public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public Student() {
     }
 
-    public Student(String fullName, String libraryId, String username, String password,
-                   String email, String phone, int soSachMuon) {
-        this.fullName = fullName;
-        this.libraryId = libraryId;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.phone = phone;
-        this.numberOfBookBorrowed = soSachMuon;
-    }
+    public String getGenderStr() { return genderStr; }
 
-    public String getFullName() {
-        return fullName;
-    }
+    public void setGenderStr(String genderStr) { this.genderStr = genderStr; }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getLibraryId() {
-        return libraryId;
-    }
-
-    public void setLibraryId(String libraryId) {
-        this.libraryId = libraryId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
+    public Student(int idStudent, String nameStudent, String genderStr, LocalDate birthDate, String phone) {
+        this.idStudent = idStudent;
+        this.nameStudent = nameStudent;
+        this.genderStr = genderStr;
+        this.birthDate = birthDate;
         this.phone = phone;
     }
 
-    public int getSoSachMuon() {
-        return numberOfBookBorrowed;
+    public static void getStudents() {
+        // Lay ma hoc sinh add vao danh sach
     }
 
-    public void setSoSachMuon(int soSachMuon) {
-        this.numberOfBookBorrowed = soSachMuon;
+    public static boolean checkStudentId(int idStudent) {
+        // Kiem tra ma hoc sinh
     }
+
+    public static boolean generateNewStudent() {
+        // Tao ma hoc sinh tu tang
+    }
+
+    public boolean addStudent(String nameStudent, String genderStr, LocalDate birthDate, String phone) {
+        // add them sinh vien
+    }
+
+    public boolean deleteStudent(int idStudent) {
+        // Xoa sinh vien
+    }
+
+    public boolean updateStudent(int idStudent, String nameStudent, String genderStr, LocalDate birthDate, String phone) {
+        // Cap nhat thong tin
+    }
+
+    public static ArrayList<Student> list_Student(int idStudent) {
+        // Lay thong tin cua mot doc gia
+    }
+
 
 }
